7b16c0358356933b152803c862b9b9cd48dada22
Teemu Spoof <tspoof@c2r5p7.hive.fi>
2022-10-27 19:43:15 +0300

    ft_memcpy NULL fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,24 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:14:46 by tspoof            #+#    #+#             */
-/*   Updated: 2022/10/27 11:00:19 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/10/27 19:41:32 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
-	while (i < n)
+	if (dst != src)
 	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
+		i = 0;
+		while (i < n)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
 	}
 	return (dst);
 }
