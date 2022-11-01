ba8f9896741c2c708e994a3903c4d5dec6685b44
Teemu Spoof <tspoof@c2r5p13.hive.fi>
2022-11-01 15:54:11 +0200

    cleaned ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,23 +6,16 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:17:37 by tspoof            #+#    #+#             */
-/*   Updated: 2022/10/26 17:12:23 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/01 15:52:00 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-
 	if (n != 0)
 	{
-		i = 0;
-		while (i < n)
-		{
-			((char *)s)[i] = 0;
-			i++;
-		}
+		ft_memset(s, 0, n);
 	}
 }
