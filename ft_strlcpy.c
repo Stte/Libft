d575522ad44c46f175d134bfd146b7168002355a
Teemu Spoof <tspoof@c2r5p7.hive.fi>
2022-11-02 14:46:54 +0200

    fixed ft_strlcpy overflow

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:36:33 by tspoof            #+#    #+#             */
-/*   Updated: 2022/10/27 17:36:01 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/02 14:43:30 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	n;
+
 	if (dstsize > 0)
 	{
-		ft_memcpy(dst, src, dstsize - 1);
-		dst[dstsize-1] = '\0';
+		n = ft_strlen(src);
+		if (n < dstsize)
+			ft_memcpy(dst, src, n + 1);
+		else
+		{
+			ft_memcpy(dst, src, dstsize - 1);
+			dst[dstsize - 1] = '\0';
+		}
 	}
 	return (ft_strlen(src));
 }
