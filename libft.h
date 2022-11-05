ffe3022634ebe79ff2660b4ebd0595b5f844a106
Teemu Spoof <steemum@hotmail.com>
2022-11-05 18:46:19 +0200

    created ft_split

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:09:24 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/05 14:06:12 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/05 17:09:49 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
