75874dd7b20dd2b2899dd9cf519e38cbdc1136e3
Teemu Spoof <steemum@hotmail.com>
2022-11-05 23:15:36 +0200

    tried to fix ft_split timeout

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 15:50:56 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/05 18:44:35 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/05 23:12:09 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int	ft_splitter(char const *s, char c, char **arr, int split_count)
 		while (s[i] && s[i] != c)
 			i++;
 		size = i - i_prev;
-		arr[arr_i] = (char *)ft_calloc(size + 1, sizeof(char));
+		arr[arr_i] = (char *)malloc((size + 1) * sizeof(char));
 		if (!arr[arr_i])
 			return (ft_free_all(arr));
 		ft_strlcpy(arr[arr_i], s + i_prev, size + 1);
@@ -75,9 +75,10 @@ char	**ft_split(char const *s, char c)
 	if (!s)
 		return (NULL);
 	split_count = ft_split_count(s, c);
-	arr = (char **)ft_calloc(split_count + 1, sizeof(char *));
+	arr = (char **)malloc((split_count + 1) *sizeof(char *));
 	if (!arr)
 		return (NULL);
+	arr[split_count + 1] = NULL;
 	if(!ft_splitter(s, c, arr, split_count))
 		return (NULL);
 	return (arr);
@@ -85,6 +86,19 @@ char	**ft_split(char const *s, char c)
 
 // int	main()
 // {
-// 	ft_split("\0aa\0bbb", '\0');
+// 	char *str = "\0aa\0bbb";
+// 	char c = '\0';
+// 	char	**expected = ft_split(str, c);
+// 	for (int i = 0; expected[i]; i++)
+// 	{
+// 		if (expected[i] != NULL)
+// 			printf("%s", expected[i]);
+// 	}
+// 	// while (result[i])
+// 	// {
+// 	// 	free(result[i]);
+// 	// 	i++;
+// 	// }
+// 	// free(result);
 // 	return (0);
 // }
