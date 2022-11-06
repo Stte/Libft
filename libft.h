07ec2e8419c2258f1b27e6bacb7f07b41f6f17e6
Teemu Spoof <steemum@hotmail.com>
2022-11-06 14:27:15 +0200

    added ft_putstr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:09:24 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/06 14:00:37 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/06 14:19:25 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
