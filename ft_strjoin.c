7abecd518103149e68ece90f6f55eb308dad0c0f
Teemu Spoof <steemum@hotmail.com>
2022-11-07 18:53:58 +0200

    ft_strjoin - protecting s2 also

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 17:39:07 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/03 18:20:05 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/07 18:53:01 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1)
+	if (!s1 || !s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
@@ -30,16 +30,3 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str[s1_len + s2_len] = '\0';
 	return (str);
 }
-
-// int	main(void)
-// {
-// 	char *s1 = "where is my ";
-// 	char *s2 = "malloc ???";
-// 	// char *s3 = "where is my malloc ???";
-
-// 	char *res = ft_strjoin(s1, s2);
-// 	if (res)
-// 		;
-
-// 	return (0);
-// }
