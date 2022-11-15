17e3b4a70e234c93433524b01aca8d2e16587e9c
Teemu Spoof <tspoof@c2r5p13.hive.fi>
2022-11-15 21:52:54 +0200

    no longer allocates longer memory than needed

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,18 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:53:11 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/05 22:00:53 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/15 21:51:23 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_min(size_t a, size_t b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -21,13 +28,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
-	if (start < s_len)
-		sub_len = len;
-	else if (s_len - start < len)
-		sub_len = s_len - start;
+	if (start <= ft_min(s_len, len))
+		sub_len = ft_min(s_len, len);
 	else
 		sub_len = 0;
-	ptr = (char *)malloc((sub_len + 1) * sizeof(char));
+	ptr = (char *)ft_calloc(sub_len + 1, sizeof(char));
 	if (!ptr)
 		return (NULL);
 	ft_strlcpy(ptr, s + start, sub_len + 1);
