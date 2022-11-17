3d6e76bf8c133051ed781ea80291434c22bce6bf
Teemu Spoof <steemum@hotmail.com>
2022-11-17 13:50:15 +0200

    tests

---
+++ test/test.c
@@ -0,0 +1,48 @@
+#include "unity/unity.h"
+#include "../libft.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+//#region ft_atoi
+void test_ft_atoi_should_be_equal_int(void) {
+	TEST_ASSERT_EQUAL_INT(42, ft_atoi("42"));
+}
+//#endregion
+
+//#region ft_isdigit
+void test_ft_isdigit_should_be_true(void) {
+	TEST_ASSERT_TRUE(ft_isdigit('1'));
+}
+void test_ft_isdigit_should_be_false(void) {
+	TEST_ASSERT_FALSE(ft_isdigit('a'));
+}
+//#endregion
+//#region ft_split
+void test_tf_split_should_be_equal(void)
+{
+	char	*string = "  	Is th|is a| real 	...| nah";
+	char	**expected = ((char*[5]){"  	Is th", "is a", " real 	...", " nah", NULL});
+	TEST_ASSERT_EQUAL_STRING_ARRAY(expected, ft_split(string, '|'), 5);
+}
+//#endregion
+
+
+int main(void)
+{
+	UNITY_BEGIN();
+	//#region ft_atoi
+	RUN_TEST(test_ft_atoi_should_be_equal_int);
+	//#endregion
+	//#region ft_isdigit
+	RUN_TEST(test_ft_isdigit_should_be_true);
+	RUN_TEST(test_ft_isdigit_should_be_false);
+	//#endregion
+	//#region ft_split
+	RUN_TEST(test_tf_split_should_be_equal);
+	//#endregion
+	return UNITY_END();
+}
