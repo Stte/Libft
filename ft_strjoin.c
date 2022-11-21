7fa81c6484d57409edfec3d71b5edaca771611f6
Teemu Spoof <tspoof@c2r4p11.hive.fi>
2022-11-21 16:56:46 +0200

    changed ft_strjoin to use ft_strlcpy

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 17:39:07 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/07 18:53:01 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/21 16:56:06 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
 	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1, s1_len);
-	ft_memcpy(str + s1_len, s2, s2_len);
+	ft_strlcpy(str, s1, s1_len + 1);
+	ft_strlcpy(str + s1_len, s2, s2_len + 1);
 	str[s1_len + s2_len] = '\0';
 	return (str);
 }
