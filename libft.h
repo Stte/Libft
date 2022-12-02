15738b3280a53120a90fa6f56ea4579db271759d
Teemu Spoof <tspoof@c2r5p13.hive.fi>
2022-12-03 00:28:43 +0200

    documentation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:09:24 by tspoof            #+#    #+#             */
-/*   Updated: 2022/12/02 23:32:37 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/12/03 00:27:48 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,28 +59,28 @@ void	*ft_calloc(size_t count, size_t size);
  */
 int		ft_isalnum(int c);
 /**
- * @brief Check if char is alphabet
+ * @brief Check if char is alphabet.
  *
  * @param c
  * @return int
  */
 int		ft_isalpha(int c);
 /**
- * @brief Check if char is ASCII
+ * @brief Check if char is ASCII.
  *
  * @param c
  * @return int
  */
 int		ft_isascii(int c);
 /**
- * @brief Check if char is digit
+ * @brief Check if char is digit.
  *
  * @param c
  * @return int
  */
 int		ft_isdigit(int c);
 /**
- * @brief Check if char is printable
+ * @brief Check if char is printable.
  *
  * @param c
  * @return int
@@ -107,7 +107,7 @@ char	*ft_itoa(int n);
  */
 void	*ft_memchr(const void *s, int c, size_t n);
 /**
- * @brief Memory compare
+ * @brief Memory compare.
  *
  * Compares \b n number of bytes from two addresses \b s1 and \b s1.
  * @param s1 First address
@@ -139,7 +139,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
  */
 void	*ft_memmove(void *dst, const void *src, size_t len);
 /**
- * @brief Fill memory area with a contant byte
+ * @brief Fill memory area with a contant byte.
  *
  * Fills \b len number of bytes of memory with \b c.
  * @param b Memory address
@@ -149,28 +149,28 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
  */
 void	*ft_memset(void *b, int c, int len);
 /**
- * @brief Writes char to file
+ * @brief Writes char to file.
  *
  * @param c
  * @param fd
  */
 void	ft_putchar_fd(char c, int fd);
 /**
- * @brief Writes new line to file
+ * @brief Writes new line to file.
  *
  * @param s
  * @param fd
  */
 void	ft_putendl_fd(char *s, int fd);
 /**
- * @brief Writes number to file
+ * @brief Writes number to file.
  *
  * @param n
  * @param fd
  */
 void	ft_putnbr_fd(int n, int fd);
 /**
- * @brief Writes string to file
+ * @brief Writes string to file.
  *
  * @param s
  * @param fd
@@ -195,7 +195,21 @@ char	**ft_split(char const *s, char c);
  * @return char* Address of the found character or NULL if not found
  */
 char	*ft_strchr(const char *s, int c);
+/**
+ * @brief Duplicates a string.
+ *
+ * @param s1 String to make a copy from
+ * @return char* Address of the new string
+ */
 char	*ft_strdup(const char *s1);
+/**
+ * @brief Calls a function for each char of a string.
+ *
+ * Function \b f takes the index and address of a character in the string.
+ *
+ * @param s String
+ * @param f Function
+ */
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 /**
  * @brief Joins two strings together.
@@ -206,13 +220,89 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
  * @attention Allocates memory
  */
 char	*ft_strjoin(char const *s1, char const *s2);
+/**
+ * @brief Adds string to end of a string.
+ *
+ * Adds source to end of the destination limited by the destination size.
+ *
+ * @param dst String
+ * @param src String
+ * @param dstsize Size of the destination
+ * @return size_t Sum of the lengths of source and destination
+ */
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+/**
+ * @brief Copies string to destination.
+ *
+ * Copies string to destination and makes sure it's null terminated.
+ *
+ * @param dst String
+ * @param src String
+ * @param dstsize Size of the destination
+ * @return size_t Lenght of the source
+ */
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+/**
+ * @brief Length of the string.
+ *
+ * @param s String
+ * @return size_t Length of the string
+ */
 size_t	ft_strlen(const char *s);
+/**
+ * @brief Create a copy of a string passing each char through a function.
+ *
+ * Creates a new string and assigns the values to that string by taking a character from the string \b s and passing it trough the function \b f.
+ * Function \b f takes a character and index of that character as parameters.
+ *
+ * @param s String
+ * @param f	Function
+ * @return char* New string
+ * @attention Allocates memory
+ */
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+/**
+ * @brief String compare.
+ *
+ * Compares two strings and returns the difference of the first missmatch character or 0 if strings are equal.
+ *
+ * @param s1 String
+ * @param s2 String
+ * @param n Number of characters to check
+ * @return int 0 if equal else the difference of the first missmatch character
+ */
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+/**
+ * @brief Search string from string.
+ *
+ * Searches string from a string and returns the address of the start of the found string.
+ *
+ * @param haystack String
+ * @param needle String
+ * @param len How many char to check
+ * @return char* Address of the found string
+ */
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+/**
+ * @brief Search a character starting from the end.
+ *
+ * Searches a character from a string and returns it's address.
+ *
+ * @param s String
+ * @param c Character
+ * @return char* Address of found char or NULL if not found
+ */
 char	*ft_strrchr(const char *s, int c);
+/**
+ * @brief Trim start and end of a string.
+ *
+ * Trims set of characters from start and end of a string.
+ *
+ * @param s1 String
+ * @param set Set of character to trim from the string
+ * @return char* Trimmed string
+ * @attention Allocates memory
+ */
 char	*ft_strtrim(char const *s1, char const *set);
 /**
  * @brief Gets a piece of string out of a string.
@@ -225,16 +315,84 @@ char	*ft_strtrim(char const *s1, char const *set);
  * @attention Allocates memory
  */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+/**
+ * @brief Changes char to lowercase.
+ *
+ * @param c
+ * @return int
+ */
 int		ft_tolower(int c);
+/**
+ * @brief Changes char to uppercase.
+ *
+ * @param c
+ * @return int
+ */
 int		ft_toupper(int c);
+/**
+ * @brief List add back.
+ *
+ * @param lst
+ * @param new
+ */
 void	ft_lstadd_back(t_list **lst, t_list *new);
+/**
+ * @brief List add front.
+ *
+ * @param lst
+ * @param new
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new);
+/**
+ * @brief Clear list.
+ *
+ * @param lst
+ * @param del
+ */
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+/**
+ * @brief Delete node.
+ *
+ * @param lst
+ * @param del
+ */
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+/**
+ * @brief Iterate list.
+ *
+ * @param lst
+ * @param f
+ */
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+/**
+ * @brief Last node in a list.
+ *
+ * @param lst
+ * @return t_list*
+ */
 t_list	*ft_lstlast(t_list *lst);
+/**
+ * @brief List map.
+ *
+ * @param lst
+ * @param f
+ * @param del
+ * @return t_list*
+ */
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+/**
+ * @brief Create a new node.
+ *
+ * @param content
+ * @return t_list*
+ */
 t_list	*ft_lstnew(void *content);
+/**
+ * @brief Size of a list.
+ *
+ * @param lst
+ * @return int
+ */
 int		ft_lstsize(t_list *lst);
 
 #endif
